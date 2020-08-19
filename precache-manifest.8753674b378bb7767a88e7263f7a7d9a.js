479e658ef11a68ae07bcd6dac9d4e46231d1d78c
sar0j <xyzpro2018@gmail.com>
2020-08-19 21:10:28 +1000

    Updates

---
+++ precache-manifest.8753674b378bb7767a88e7263f7a7d9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "310eb82444233cc06703594f60adca1d",
+    "url": "/smartbrain/index.html"
+  },
+  {
+    "revision": "1a1b76304052df4aec9c",
+    "url": "/smartbrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9e1cde30ab08ca461f13",
+    "url": "/smartbrain/static/css/main.23d76e71.chunk.css"
+  },
+  {
+    "revision": "1a1b76304052df4aec9c",
+    "url": "/smartbrain/static/js/2.e1f0ce76.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/smartbrain/static/js/2.e1f0ce76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e1cde30ab08ca461f13",
+    "url": "/smartbrain/static/js/main.5bc3f03e.chunk.js"
+  },
+  {
+    "revision": "85bdf2fccc27b5b7f8ed",
+    "url": "/smartbrain/static/js/runtime-main.c7c75b64.js"
+  }
+]);
